0962bc14f65eb44bab527d1c6f98ba518dafaf8d
KarinaKano13 <75265602+KarinaKano13@users.noreply.github.com>
2021-05-21 11:36:51 -0500

    actualización regresando al commit anterior

--- src/ComponentView/generalComponents/Header.js
+++ src/ComponentView/generalComponents/Header.js
@@ -1,7 +1,8 @@
+  
 import React from "react";
 import totallook from '../../Assets/logo/totalLook.png'
 // npm install --save-dev @iconify/react @iconify-icons/heroicons-outline
-import { Icon, InlineIcon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import shoppingCart from '@iconify-icons/heroicons-outline/shopping-cart';
 
 
@@ -10,10 +11,15 @@ export function Header() {
     return(
 
         <div className="header">
-            <div className="header-content">
+
+            <div className='logoImage'>
                 <img src={totallook} alt="totallook" />
-                <Icon icon={shoppingCart} color="#ff9f1c" width="30px" height="30px" />
             </div>
+            <div className='carElements'>
+            <p className='miCarritoText'>Mi Carrito</p>
+                <Icon className='iconShoppingCar' icon={shoppingCart} color="#ff9f1c" width="50px" height="50px" />
+            </div>            
+            
         </div>
 
     )

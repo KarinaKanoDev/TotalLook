af723e23221c17a9b9bf9f3d2d3c5b42b4e8e6d4
Mariliz G <marilizgonzalez1@gmail.com>
2021-05-21 11:07:55 -0500

    Creación de componente ProductContainer

--- src/ComponentView/productPage/Product.jsx
+++ src/ComponentView/productPage/Product.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import {Footer} from '../generalComponents/Footer'
 import {Header} from '../generalComponents/Header'
+import { ProductContainer } from './ProductContainer';
+
 
 // import { Icon, InlineIcon } from '@iconify/react';
 // import searchIcon from '@iconify-icons/fa-solid/search';
@@ -17,77 +19,11 @@ export const Product = ({product}) => {
                 <p> Productos </p>
                 <input type='text' className='searchBar' placeholder='buscar'></input>
             </div>
-        <div className='productsElementContainer'>
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo1.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
+            <div className='productComponents'>
+            <ProductContainer/>
+            <ProductContainer/>
+            <ProductContainer/>
             </div>
-            </div>
-
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo2.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo3.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo1.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo2.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo3.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo1.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo2.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-
-            <div className='productCard'>
-            <div className='productsElementImage'>
-                <img className='productImg' src='./sampleImage/ropamujermodelo3.png' alt='ropa'/>
-                <div className='productsElementName'>Blusa </div>
-                <div className='productsElementPrice'> $400 </div>
-            </div>
-            </div>
-        </div>
             <Footer />
 
         </div>
